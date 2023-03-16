4a90cf505fe814064d39aae696912dbdf1e73efb
Fabio Bonelli <fb@fabiobonelli.it>
2023-03-16 21:05:12 +0100

    refactor: format index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,40 @@
 import React from 'react';
 
+const loremIpsum = `
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra,
+  nibh ut ullamcorper euismod, dui ligula imperdiet purus, id porttitor
+  ligula metus nec tortor. Sed fermentum dui nec pulvinar congue. Morbi laoreet
+  id dolor vitae dapibus. Duis id rhoncus dui.
+`;
+
 const Page = () => (
   <div className="container">
     <div className="row">
       <div className="col-md-6">
         <h2>Cosa è l'ecosistema interoperabilità</h2>
         <p>
-          Abilita lo scambio semplice e sicuro di informazioni tra tutte le Pubbliche amministrazioni per incrementare l'efficienza dell'azione amministrativa, ridurre la richiesta di dati al cittadino e creare nuove opportunità di sviluppo per le imprese.
-+ value proposition
+          Abilita lo scambio semplice e sicuro di informazioni tra tutte le
+          Pubbliche amministrazioni per incrementare l'efficienza
+          dell'azione amministrativa, ridurre la richiesta di dati al
+          cittadino e creare nuove opportunità di sviluppo per le imprese.
+          + value proposition
         </p>
       </div>
-
       <div className="col-md-6">
         <h3>MODI</h3>
-        <p>
-           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra, nibh ut ullamcorper euismod, dui ligula imperdiet purus, id porttitor ligula metus nec tortor. Sed fermentum dui nec pulvinar congue. Morbi laoreet id dolor vitae dapibus. Duis id rhoncus dui.
-        </p>
-
+        <p>{loremIpsum}</p>
         <h3>BDIN</h3>
-        <p>
-           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra, nibh ut ullamcorper euismod, dui ligula imperdiet purus, id porttitor ligula metus nec tortor. Sed fermentum dui nec pulvinar congue. Morbi laoreet id dolor vitae dapibus. Duis id rhoncus dui.
-        </p>
-        <button type="button" class="btn btn-primary">CTA</button>
+        <p>{loremIpsum}</p>
+        <button type="button" className="btn btn-primary">CTA</button>
       </div>
     </div>
-
     <h3>Come funziona</h3>
     <div className="row">
       <div className="col-md-3 border-right">
         <h4>1. RICERCA</h4>
         <p>
-          Qual è il processo di interoperabilità e attraverso cosa si fa interoperabilità (PDND + Schema)
+          Qual è il processo di interoperabilità e attraverso cosa si fa
+          interoperabilità (PDND + Schema)
         </p>
         <p><a href="#">Link (Catalogo)</a></p>
         <p><a href="#">Link (Schema)</a></p>
@@ -38,46 +42,43 @@ const Page = () => (
       <div className="col-md-3 border-right">
         <h4>2. SVILUPPA</h4>
         <p>
-          Qual è il processo di interoperabilità e attraverso cosa si fa interoperabilità (PDND + Schema)
+          Qual è il processo di interoperabilità e attraverso cosa si fa
+          interoperabilità (PDND + Schema)
         </p>
         <p><a href="#">Link sezione piattaforma PDND su Developers Italia</a></p>
       </div>
       <div className="col-md-3 border-right">
         <h4>3. VALIDA</h4>
         <p>
-          Qual è il processo di interoperabilità e attraverso cosa si fa interoperabilità (PDND + Schema)
+          Qual è il processo di interoperabilità e attraverso cosa si fa
+          interoperabilità (PDND + Schema)
         </p>
         <p><a href="#">Link validatore</a></p>
       </div>
       <div className="col-md-3 border-right">
         <h4>4. PUBBLICA</h4>
         <p>
-          Qual è il processo di interoperabilità e attraverso cosa si fa interoperabilità (PDND + Schema)
+          Qual è il processo di interoperabilità e attraverso cosa si fa
+          interoperabilità (PDND + Schema)
         </p>
         <p><a href="#">Link (pagoPa Piattaforme)</a></p>
       </div>
     </div>
-
     <h3>Attraverso cosa</h3>
     <div className="row">
       <div className="col-md-6">
-        <p>Qual è il processo di interoperabilità e attraverso cosa si fa interoperabilità (PDND + Schema)</p>
+        <p>Qual è il processo di interoperabilità e attraverso cosa si fa
+        interoperabilità (PDND + Schema)</p>
         <h3>PDND</h3>
-        <p>
-           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra, nibh ut ullamcorper euismod, dui ligula imperdiet purus, id porttitor ligula metus nec tortor. Sed fermentum dui nec pulvinar congue. Morbi laoreet id dolor vitae dapibus. Duis id rhoncus dui.
-        </p>
-        <button type="button" class="btn btn-primary">CTA</button>
+        <p>{loremIpsum}</p>
+        <button type="button" className="btn btn-primary">CTA</button>
 
         <h3>SCHEMA</h3>
-        <p>
-           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra, nibh ut ullamcorper euismod, dui ligula imperdiet purus, id porttitor ligula metus nec tortor. Sed fermentum dui nec pulvinar congue. Morbi laoreet id dolor vitae dapibus. Duis id rhoncus dui.
-        </p>
-        <button type="button" class="btn btn-primary">CTA</button>
+        <p>{loremIpsum}</p>
+        <button type="button" className="btn btn-primary">CTA</button>
 
         <h3>CATALOGO API</h3>
-        <p>
-           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra, nibh ut ullamcorper euismod, dui ligula imperdiet purus, id porttitor ligula metus nec tortor. Sed fermentum dui nec pulvinar congue. Morbi laoreet id dolor vitae dapibus. Duis id rhoncus dui.
-        </p>
+        <p>{loremIpsum}</p>
         <p><a href="#">CTA LINK</a></p>
       </div>
       <div className="col-md-6">
@@ -91,29 +92,21 @@ const Page = () => (
         <div className="col-md-6">
           <div className="row">
             <h4>Cittadino</h4>
-            <p>
-               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra, nibh ut ullamcorper euismod, dui ligula imperdiet purus, id porttitor ligula metus nec tortor. Sed fermentum dui nec pulvinar congue. Morbi laoreet id dolor vitae dapibus. Duis id rhoncus dui.
-            </p>
+            <p>{loremIpsum}</p>
           </div>
           <div className="row">
             <h4>Sviluppatori</h4>
-            <p>
-               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra, nibh ut ullamcorper euismod, dui ligula imperdiet purus, id porttitor ligula metus nec tortor. Sed fermentum dui nec pulvinar congue. Morbi laoreet id dolor vitae dapibus. Duis id rhoncus dui.
-            </p>
+            <p>{loremIpsum}</p>
           </div>
         </div>
         <div className="col-md-6">
           <div className="row">
             <h4>Ente fornitore e fruitore</h4>
-            <p>
-               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra, nibh ut ullamcorper euismod, dui ligula imperdiet purus, id porttitor ligula metus nec tortor. Sed fermentum dui nec pulvinar congue. Morbi laoreet id dolor vitae dapibus. Duis id rhoncus dui.
-            </p>
+            <p>{loremIpsum}</p>
           </div>
           <div className="row">
             <h4>DPO</h4>
-            <p>
-               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra, nibh ut ullamcorper euismod, dui ligula imperdiet purus, id porttitor ligula metus nec tortor. Sed fermentum dui nec pulvinar congue. Morbi laoreet id dolor vitae dapibus. Duis id rhoncus dui.
-            </p>
+            <p>{loremIpsum}</p>
           </div>
         </div>
       </div>
@@ -121,10 +114,7 @@ const Page = () => (
 
     <div className="row text-center justify-content-center">
       <h3 className="h2">Supporto</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra, nibh ut ullamcorper euismod, dui ligula imperdiet purus, id porttitor ligula metus nec tortor. Sed fermentum dui nec pulvinar congue. Morbi laoreet id dolor vitae dapibus. Duis id rhoncus dui.
-      </p>
-
+      <p>{loremIpsum}</p>
       <section>
       <div className="container">
         <div className="row row-eq-height">
@@ -167,7 +157,6 @@ const Page = () => (
                     </article>
                 </a>
             </div>
-
         </div>
       </div>
     </section>
